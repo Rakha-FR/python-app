f230bd9581b90872b0c05c08b3254dd5c52de19e
Rakha Fadilah <raka.fadhilah123@gmail.com>
2025-11-14 21:49:23 +0700

    ganti format env pada class config

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY src/ .
 
-RUN cd src
-
 CMD ["python", "app.py"]
--- src/config.py
+++ src/config.py
@@ -1,11 +1,13 @@
+import os
+
 class Config:
-    DB_HOST = "postgres"
-    DB_NAME = "app_nilai"
-    DB_USER = "app_be"
-    DB_PASSWORD = "app#123" 
-    DB_PORT = 5443
+    DB_HOST = os.getenv("DB_HOST", "postgres_db")
+    DB_NAME = os.getenv("DB_NAME", "app_nilai")
+    DB_USER = os.getenv("DB_USER", "app_be")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "app#123")
+    DB_PORT = int(os.getenv("DB_PORT", 5443))
     
     # Flask config
-    DEBUG = True
-    HOST = "0.0.0.0"
-    PORT = 5000
+    DEBUG = bool(os.getenv("DEBUG", "true").lower() == "true")
+    HOST = os.getenv("HOST", "0.0.0.0")
+    PORT = int(os.getenv("PORT", 5000))
